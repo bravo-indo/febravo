f01b7ce4ee3b4fdd0dd471eb38f4030f53ee3855
Eka Fajhari Adwar <eka.adwar@gmail.com>
2021-07-03 14:14:49 +0700

    add style for mobile device in UserCard components, change style in contents of Home Page

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -8,9 +8,9 @@ function UserCard({
   img, name, role, location, skill, to
 }) {
   return (
-    <div className=" flex flex-row space-x-8 px-5 py-10 items-center border-b-2 border-gray-200">
-      <img src={img} alt="user" className="w-24 h-24" />
-      <div className="space-y-2 flex-1">
+    <div className=" flex flex-col md:flex-row space-y-10 md:space-y-0 md:space-x-8 px-5 py-10 items-center border-b-2 border-gray-200">
+      <img src={img} alt="user" className="w-20 md:w-24 h-20 md:h-24" />
+      <div className="flex flex-col items-center space-y-2 flex-1">
         <h4 className="text-2xl font-semibold">{name}</h4>
         <h5 className="text-gray-400">{role}</h5>
         <div className="flex flex-row items-center space-x-1">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,25 +16,26 @@ function Home({auth,getProfileData}) {
 
   return (
     <main>
-      <section className="bg-purple-800 py-5 px-32">
+      <section className="bg-purple-800 py-5 lg:px-32">
         <p className="px-5 text-white text-2xl font-semibold">Top Jobs</p>
       </section>
       <section className="bg-gray-100 md:px-32 py-5">
-        <div className="flex flex-row  items-center space-x-5 bg-white md:px-4 py-1 rounded-lg">
+        <div className="flex flex-row  items-center space-x-5 bg-white px-4 py-1 rounded-lg">
             <input
             type="text"
             placeholder="Search for any skill"
             className="p-4 flex-1 w-96 focus:outline-none"
             />
-              <BsSearch size={25} />
-            <select className="focus:outline-none">
+            <BsSearch size={25} />
+            <select className="hidden md:inline-block focus:outline-none">
               <option value="name">Sortir berdasarkan nama</option>
               <option value="skill">Sortir berdasarkan Skill</option>
               <option value="location">Sortir berdasarkan Lokasi</option>
               <option value="freelance">Sortir berdasarkan freelance</option>
               <option value="fulltime">Sortir berdasarkan fulltime</option>
             </select>
-            <Button label="Search" isPrimary />
+            {/* <Button className="hidden md:inline-block " label="Search" isPrimary /> */}
+            <button className="hidden md:inline-block rounded-md py-3 px-4 bg-purple-800 text-white border-2 border-purple-800 font-semibold">Search</button>
         </div>
       </section>
       <section className="bg-gray-100 md:px-32">
@@ -73,19 +74,19 @@ function Home({auth,getProfileData}) {
           <button className="bg-white p-5 text-gray-400 font-semibold ">
             1
           </button>
-          <button className="bg-white p-5 text-gray-400 font-semibold ">
+          <button className="hidden md:inline-block bg-white p-5 text-gray-400 font-semibold ">
             2
           </button>
-          <button className="bg-white p-5 text-gray-400 font-semibold ">
+          <button className="hidden md:inline-block bg-white p-5 text-gray-400 font-semibold ">
             3
           </button>
-          <button className="hidden bg-white p-5 text-gray-400 font-semibold ">
+          <button className="hidden lg:inline-block bg-white p-5 text-gray-400 font-semibold ">
             4
           </button>
-          <button className="hidden bg-white p-5 text-gray-400 font-semibold ">
+          <button className="hidden lg:inline-block bg-white p-5 text-gray-400 font-semibold ">
             5
           </button>
-          <button className="hidden bg-white p-5 text-gray-400 font-semibold ">
+          <button className="hidden lg:inline-block bg-white p-5 text-gray-400 font-semibold ">
             6
           </button>
           <button className="bg-white p-5 text-gray-400 font-semibold ">
