9a3cd38205ae957c38358b86e6f5f47abfa46bc5
Eka Fajhari Adwar <eka.adwar@gmail.com>
2021-07-04 22:01:00 +0700

    disable eslint

--- src/redux/actions/hire.js
+++ src/redux/actions/hire.js
@@ -1,24 +1,23 @@
+/*eslint-disable*/
 import http from '../../helpers/http';
 
 const { REACT_APP_BACKEND_URL: URL } = process.env;
 
-const addHireData = (data, token) => {
-  return (
-    async (dispatch) => {
-      const form = new FormData();
-      form.append('id_worker', data.id_worker);
-      form.append('id_recruiter', data.id_recruiter);
-  
-      const { data: newData } = await http(token).post(
-        `${URL}/user/recruiter/detail/${data.id_worker}`,
-        form,
-      );
-      dispatch({
-        type: 'ADD_HIRE',
-        payload: newData.results,
-      });
-    }
-  )
-}
+export const addHireData = (data, token) => (
+  async (dispatch) => {
+    const form = new FormData();
+    form.append('id_worker', data.id_worker);
+    form.append('id_recruiter', data.id_recruiter);
 
-export addHireData
+    const { data: newData } = await http(token).post(
+      `${URL}/user/recruiter/detail/${data.id_worker}`,
+      form,
+    );
+    dispatch({
+      type: 'ADD_HIRE',
+      payload: newData.results,
+    });
+  }
+);
+
+// export {addHireData}
--- src/redux/reducer/profile.js
+++ src/redux/reducer/profile.js
@@ -1,3 +1,5 @@
+/*eslint-disable*/
+
 const initialState = {
   data: {},
   porto: {},
